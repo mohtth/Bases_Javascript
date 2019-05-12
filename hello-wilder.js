a51e9c3eae4000c08001b2a369e112e9d1deebbf
mohtth <48013101+mohtth@users.noreply.github.com>
2019-05-12 17:13:10 +0200

    Update hello-wilder.js

--- hello-wilder.js
+++ hello-wilder.js
@@ -2,8 +2,8 @@
 
 // Define some variables
 const title = 'The Godfather';
-const releaseDate = October 18, 1972 ;
-const director = ' Francis Ford Coppola';
+const releaseDate = 'October 18, 1972' ;
+const director = 'Francis Ford Coppola';
 const message = title + ', is a film directed by ' + director + ' and released in ' + releaseDate;
 
 // Display a popup
